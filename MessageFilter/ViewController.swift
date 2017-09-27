e94c053567ca685e02e366715d22d8a083ff516c
Jia <jia.tian@me.com>
2017-09-27 20:18:22 +0800

    init

---
+++ MessageFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessageFilter
+//
+//  Created by Jia Tian on 27/09/2017.
+//  Copyright © 2017 Jia Tian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
